abde4979be7baa71202ae85103e570f12e6ea1d4
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-21 12:24:29 +0900

    Update warp.py

--- sol1/warp.py
+++ sol1/warp.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 # Copyright 2018 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
